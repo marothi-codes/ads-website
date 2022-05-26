430d6d1585e8e40c89fdaf003d86bdec29cfbce4
Marothi Mahlake <code_artist@outlook.com>
2022-05-26 17:46:23 +0200

    Colours adjusted to match ADS branding colours.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,15 +1,15 @@
 export default {
   // example colors with dark mode
   colors: {
-    text: '#343D48', // body color and primary color
+    text: '#000', // body color and primary color
     text_secondary: '#02073E', // secondary body color
     heading: '#0F2137', // primary heading color
-    heading_secondary: '#0F2137', // heading color
+    heading_secondary: '#d52028', // heading color
     background: '#FFFFFF', // body background color
     background_secondary: '#F9FBFD', // secondary background color
     border_color: '#E5ECF4', // border color
     yellow: '#FFA740', // border color
-    primary: '#EA3A60', // primary button and link color
+    primary: '#d52028', // primary button and link color
     secondary: '#2563FF', // secondary color - can be used for hover states
     muted: '#E4E4E4', // muted color
     accent: '#609', // a contrast color for emphasizing UI
