33e570af04271e5e4bf83cbf088d5669881037aa
Marothi Mahlake <code_artist@outlook.com>
2022-05-30 14:55:33 +0200

    Controlled form implemented on contact section.

--- src/sections/contact-section.js
+++ src/sections/contact-section.js
@@ -8,7 +8,7 @@ import theme from 'theme';
 export default function ContactSection() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [enquiry, setEnquiry] = useState('');
+  const [phone, setPhone] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
 
@@ -25,32 +25,71 @@ export default function ContactSection() {
         <SectionHeader slogan="LET US SOLVE YOUR PROBLEM" title="Contact Amanzunza" />
         <hr sx={theme.styles.hr} />
         <br />
-        <Box as="form" onSubmit={(e) => e.preventDefault()}>
+        <Box as="form" onSubmit={(e) => handleFormSubmit(e)}>
           {/* Name */}
           <Label sx={styles.forms.label} htmlFor="name">
             Name:
           </Label>
-          <Input sx={styles.forms.input} name="name" id="name" mb={3} />
+          <Input
+            sx={styles.forms.input}
+            name="name"
+            id="name"
+            mb={3}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           {/* Email */}
           <Label sx={styles.forms.label} htmlFor="email">
             Email:
           </Label>
-          <Input sx={styles.forms.input} type="email" name="email" id="email" mb={3} />
-          {/* Enquiry */}
-          <Label sx={styles.forms.label} htmlFor="enquiry">
-            Enquiry:
+          <Input
+            sx={styles.forms.input}
+            type="email"
+            name="email"
+            id="email"
+            mb={3}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* Phone */}
+          <Label sx={styles.forms.label} htmlFor="phone">
+            Phone:
           </Label>
-          <Input sx={styles.forms.input} type="enquiry" name="enquiry" id="enquiry" mb={3} />
+          <Input
+            sx={styles.forms.input}
+            type="phone"
+            name="phone"
+            id="phone"
+            mb={3}
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
           {/* Subject */}
           <Label sx={styles.forms.label} htmlFor="subject">
             Subject:
           </Label>
-          <Input sx={styles.forms.input} type="subject" name="subject" id="subject" mb={3} />
+          <Input
+            sx={styles.forms.input}
+            type="subject"
+            name="subject"
+            id="subject"
+            mb={3}
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
           {/* Message */}
           <Label sx={styles.forms.label} htmlFor="message">
             Message:
           </Label>
-          <Textarea sx={styles.forms.textarea} name="message" id="message" rows={6} mb={3} />
+          <Textarea
+            sx={styles.forms.textarea}
+            name="message"
+            id="message"
+            rows={6}
+            mb={3}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
           {/* Submit Button */}
           <hr sx={theme.styles.hr} />
           <Button type="submit">Send my Message</Button>
